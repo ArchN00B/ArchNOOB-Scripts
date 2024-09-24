a9c87b5f5b950b66592eedc29200238741268abe
ArchN00B <archn00b2024@gmail.com>
2024-09-23 21:10:29 -0400

    last commit

--- starship-script/install.sh
+++ starship-script/install.sh
@@ -5,12 +5,7 @@
 printf "%s\n" " Installing Starship."
 [ -f x86_64.txt ] && sudo pacman -S - < x86_64.txt --noconfirm --needed 
 
-# install Fira Nerd Font 
-wget -P FiraCode https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/FiraCode.zip
-
-[ -f FiraCode/FiraCode.zip  ] && cd FiraCode
-unzip FiraCode.zip && rm -r FiraCode.zip 
-cd .. && sudo mv -vf FiraCode /usr/share/fonts/ 
+sudo cp -rf FiraCode /usr/share/fonts/ 
 [ -f ~/.bashrc ] && cp  ~/.bashrc ~/.bashrc.bak."$(date +"%Y%m%d_%H%M%S")"
 [ -f ~/.bashrc ] && \
 echo '
