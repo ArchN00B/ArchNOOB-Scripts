0e26521692d9ace6d18380d65e583dfc2bfbedf3
archn00b <archn00b2024@gmail.com>
2024-10-10 07:44:43 +0000

    color

--- pushit2git.sh
+++ pushit2git.sh
@@ -19,15 +19,32 @@
 #tput setaf 8 = light blue
 ##################################################################################################################
 
+# printing to the screen using color
 printf "%s\033[34mChecking FOR UPDATED FILES ONE REPO\033[m\n"
+
+# Checking for updated files on repo
 git pull
+
+# Adding edited files to repo . means pwd
 git add .
-sleep 3
+sleep 1
+
+# Checking the status of edited files
 git status
+
+# Printing to the screen with color
 printf "%s\033[34mADDING EDITED FILES TO REPO MAKE YOUR COMMIT\033[m\n"
+
+# Read user input very important to add commit or it failed to add edited files to repo
 read input
+
+# Adding the commit to repo
 git commit -m "$input"
+
+# Printing color to the screen 
 printf "%s\033[34mFINISHING UP ALMOST DONE\033[m\n"
+
+# Pushing edited files to master repo
 git push -u origin master
 
 
